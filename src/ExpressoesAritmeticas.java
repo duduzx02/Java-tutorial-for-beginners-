9f9a52efc0438dd7697d98e9ee717d6e6dbe2d14
Eduardo Lima <duduzx02@gmail.com>
2024-02-05 03:33:03 -0300

    expressões aritméticas

---
+++ src/ExpressoesAritmeticas.java
@@ -0,0 +1,20 @@
+public class ExpressoesAritmeticas {
+    public static void main(String[] args) {
+        // expressão = operandos e operadores
+        // Operandos = valores, variáveis, números e quantidas
+        // Operadores = + - * / %
+
+        // Exemplos
+        int friends = 10;
+        friends = friends + 1;
+
+        System.out.println(friends);
+
+        friends--;
+        System.out.println(friends);
+
+
+
+
+    }
+}
