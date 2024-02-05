3b71a555bfb773303e04a0b34c01f4e93a8aa634
Eduardo Lima <duduzx02@gmail.com>
2024-02-05 15:55:07 -0300

    Declarações if - if else - else

---
+++ src/DeclaracoesIf.java
@@ -0,0 +1,12 @@
+public class DeclaracoesIf {
+    public static void main(String[] args) {
+        int idade = 18;
+        if (idade >= 75){
+            System.out.println("OK, boomer!!");
+        } else if(idade >= 18){
+            System.out.println("Você é um adulto!!");
+        } else {
+            System.out.println("Você não é adulto!!");
+        }
+    }
+}
