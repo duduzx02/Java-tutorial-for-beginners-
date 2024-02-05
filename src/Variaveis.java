93092ab5f055bb25ae20bf5dcd556ab60b88591d
Eduardo Lima <duduzx02@gmail.com>
2024-02-05 01:54:22 -0300

    Váriave3is em java: Uma introdução Abrangente

---
+++ src/Variaveis.java
@@ -0,0 +1,29 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Du";
+
+        System.out.println("Hello " + name);
+
+        // Declaração e atribução separadas
+        int numero;
+        numero = 42;
+
+        // Inicialização
+        int novoNumero = 99;
+
+        int idade = 30;
+        double altura = 1.80;
+        String nome = "Ana";
+
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+
+    }
+}
