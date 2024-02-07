dea6582b6a621c8cc45225c21d44083f99c7f12b
Eduardo Lima <duduzx02@gmail.com>
2024-02-07 03:30:33 -0300

    For loop

---
+++ src/ForLoop.java
@@ -0,0 +1,18 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i =0; i<= 10; i++){
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+
+        System.out.println("Feliz ano novo!!");
+
+        for (int i = 0; i <= 10; i = i + 2){
+            System.out.println(i);
+        }
+    }
+}
