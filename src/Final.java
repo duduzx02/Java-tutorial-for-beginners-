e6f658eaf7a1c712ac5be96442b921e2bea993c5
Eduardo Lima <duduzx02@gmail.com>
2024-02-09 04:27:37 -0300

    Palavra chave final

---
+++ src/Final.java
@@ -0,0 +1,10 @@
+public class Final {
+
+    public static void main(String[] args) {
+        final double pi = 3.14;
+
+        // pi = 4; não é possivel
+
+        System.out.println(pi);
+    }
+}
