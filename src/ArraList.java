d7c1b0aac2690ef005290156583b016c0d20a8b7
Eduardo Lima <duduzx02@gmail.com>
2024-02-08 23:55:25 -0300

    ArrayList

---
+++ src/ArraList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class ArraList {
+
+    public static void main(String[] args) {
+        ArrayList<String> food = new ArrayList<>();
+        System.out.println("food.add(\"Pizza\");" + food.add("Pizza"));
+        System.out.println("food.add(\"Hamburger\");" + food.add("Hamburguer"));
+        System.out.println("food.add(\"Hotdog\");" + food.add("hotdog"));
+
+        System.out.println(food);
+        for (int i = 0; i < food.size(); i++) {
+            System.out.println("food.get(i)); = " + food.get(i));
+        }
+
+        System.out.println("food.set(2,\"Sushi\");" + food.set(2, "Sushi"));
+        // set() substitu um elemento em um índice específico.
+
+        System.out.println(food);
+
+        System.out.println("food.remove(2)" + food.remove(2));
+        // remove() remove um elemento em um índice específico
+
+        System.out.println(food);
+
+        System.out.println("food.clear()");
+        food.clear();
+        // clear() remove todos os elementos do ArrayList
+
+        System.out.println(food);
+
+    }
+}
---
+++ src/Wrraper.java
@@ -0,0 +1,23 @@
+public class Wrraper {
+    public static void main(String[] args) {
+
+        // wrapper classes
+        // As classes que são wrapper tem uma vantagem por conta dos métodos, porém, uma desvantagem por conta de ser
+        // de precisar de mais processamento
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Eud";
+
+        // tipo primitivo
+        // vanntagens e desvantagem é o contrário das classes wrappr
+        boolean aa = true;
+
+        if(aa==a){
+            System.out.println("This is a true;");
+        }
+
+    }
+}
