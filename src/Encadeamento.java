de68b0bcb6d80db5fc377a55b21c3b6217df0816
Eduardo Lima <duduzx02@gmail.com>
2024-03-01 09:55:51 -0300

    Encadeamento de métodos

---
+++ src/Encadeamento.java
@@ -0,0 +1,9 @@
+public class Encadeamento {
+    public static void main(String[] args) {
+        String nome = "     Bro";
+
+        nome = nome.concat(" code         ").toUpperCase().trim();
+
+        System.out.println(nome);
+    }
+}
